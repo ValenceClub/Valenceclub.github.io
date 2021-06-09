6794354e97ced7413696e3ee91eebf4447212d41
SanchayAgarwal <85455971+SanchayAgarwal@users.noreply.github.com>
2021-06-09 21:52:27 +0530

    random

--- js/init.js
+++ js/init.js
@@ -6,3 +6,37 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+    
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tap-target');
+  var instances = M.TapTarget.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.tap-target').tapTarget();
+});
+      
