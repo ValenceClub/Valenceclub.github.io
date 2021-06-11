9bd470c53c5b8f58395ba9d40d211313057a355f
SanchayAgarwal <85455971+SanchayAgarwal@users.noreply.github.com>
2021-06-11 11:43:56 +0530

    sessions1

--- js/materialize.js
+++ js/materialize.js
@@ -10439,10 +10439,10 @@ $jscomp.polyfill = function (e, r, p, m) {
     duration: 200, // ms
     dist: -100, // zoom scale TODO: make this more intuitive as an option
     shift: 0, // spacing for center image
-    padding: 0, // Padding between non center items
+    padding: -10, // Padding between non center items
     numVisible: 5, // Number of visible items in carousel
     fullWidth: false, // Change to full width styles
-    indicators: false, // Toggle indicators
+    indicators: true, // Toggle indicators
     noWrap: false, // Don't wrap around and cycle through items.
     onCycleTo: null // Callback for when a new slide is cycled to.
   };
